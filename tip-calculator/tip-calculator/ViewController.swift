99b5ff411607a055b55bd6fbeb88ec4452cc619b
Phaninder Kumar <phaninderkumar@macOS-PhaniTests-MacBook-Air.local>
2023-12-06 20:45:18 +0530

    Setup the inital project and initialize the root view controller programmatically

---
+++ tip-calculator/tip-calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tip-calculator
+//
+//  Created by Phaninder Kumar on 06/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .yellow
+    }
+
+
+}
+
