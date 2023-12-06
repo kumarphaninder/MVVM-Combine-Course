6518598a93326e0cdaed7f83e47a86b3e5b27a12
Phaninder Kumar <phaninderkumar@macOS-PhaniTests-MacBook-Air.local>
2023-12-06 21:07:13 +0530

    Added basic placeholder views for the UI

---
+++ tip-calculator/tip-calculator/View/LogoView.swift
@@ -0,0 +1,27 @@
+//
+//  LogoView.swift
+//  tip-calculator
+//
+//  Created by Phaninder Kumar on 06/12/23.
+//
+
+import UIKit
+
+class LogoView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        backgroundColor = .red
+    }
+}
+
+
+
---
+++ tip-calculator/tip-calculator/View/ResultView.swift
@@ -0,0 +1,24 @@
+//
+//  ResultView.swift
+//  tip-calculator
+//
+//  Created by Phaninder Kumar on 06/12/23.
+//
+
+import UIKit
+
+class ResultView: UIView {
+    
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func layout() {
+        backgroundColor = .green
+    }
+}
